3d9708472c1a5c9dbd8248178716ac858185029d
zhipeng <u6766505@anu.edu.au>
2023-09-14 01:36:27 +0800

    Added streamlit modules

--- streamlit/streamlit_NSGAN_web.py
+++ streamlit/streamlit_NSGAN_web.py
@@ -116,7 +116,7 @@ ArXiv paper can be access at: 'link'
 """)
 
 # Add the selection box with default values for tensile and elongation
-objective_choices = ['Tensile Strength', 'Elongation', 'Yield Strength', 'Density', 'Hardness', 'FCC', 'BCC', 'HCP',
+objective_choices = ['Tensile Strength', 'Elongation', 'Yield Strength', 'Hardness', 'Density', 'FCC', 'BCC', 'HCP',
                      'IM', 'Aluminum content']
 selected_objectives = st.multiselect('Choose objectives for optimization:', objective_choices,
                                      default=['Tensile Strength', 'Elongation', 'FCC'])
